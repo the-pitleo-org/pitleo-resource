2f0b660f34ad0153daf145d79237f41d3236f3b7
Maki <makidevhelp@gmail.com>
2025-07-25 10:50:09 -0700

    0.16974653950772667

--- run.py
+++ run.py
@@ -1,9 +1,17 @@
 
 import os
 import random
+from time import sleep
+sleep(1)
 try:
     os.system("git add .")
+    print("git add .")
+    sleep(1)
     os.system(f'git commit -m "{random.random()}"')
+    print("Created a commit")
     os.system("git push")
+    print("Pushed")
 except:
+    sleep(1)
     print("An error occured.")
+    input("Press enter to exit")
