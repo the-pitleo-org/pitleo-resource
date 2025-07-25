0303c1fe60bfd356b53b996ef541a9f62d94e05f
Maki <makidevhelp@gmail.com>
2025-07-24 22:42:18 -0700

    hi

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ print("Publishing!")
 import os
 try:
     os.system("git add .")
-    os.system("git commit -m 'New Update.'")
+    os.system('git commit -m "hi"')
     os.system("git push")
 except:
     print("An error occured.")
